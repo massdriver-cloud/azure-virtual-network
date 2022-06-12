cef3f04a8ba530ed67b33be8ae81175a30bf6580
chrisghill <chris@massdriver.cloud>
2022-06-11 19:50:02 -0600

    remove delegated subnets

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -7,19 +7,9 @@ resource "massdriver_artifact" "vnet" {
     {
       data = {
         infrastructure = {
-          vnet_id           = azurerm_virtual_network.main.id
+          id                = azurerm_virtual_network.main.id
           cidr              = var.cidr
           default_subnet_id = azurerm_subnet.main.id
-          delegated_subnets = {
-            "Microsoft.DBforPostgreSQL/flexibleServers" = {
-              subnet_id           = azurerm_subnet.postgresql_flexible.0.id
-              private_dns_zone_id = azurerm_private_dns_zone.postgresql_flexible.0.id
-            }
-            "Microsoft.DBforMySQL/flexibleServers" = {
-              subnet_id           = azurerm_subnet.mysql_flexible.0.id
-              private_dns_zone_id = azurerm_private_dns_zone.mysql_flexible.0.id
-            }
-          }
         }
         observability = {
           alarm_monitor_action_group_ari = azurerm_monitor_action_group.main.id
