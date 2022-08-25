cc0e9a5ac6937d0858bf942e5c0ce414dd92a4d1
mclacore <michael@massdriver.cloud>
2022-08-25 14:37:51 -0600

    Adding tagging for compliance

--- src/main.tf
+++ src/main.tf
@@ -1,6 +1,8 @@
 resource "azurerm_resource_group" "main" {
   name     = var.md_metadata.name_prefix
   location = var.azure_region
+
+  tags = var.md_metadata.default_tags
 }
 
 resource "azurerm_virtual_network" "main" {
