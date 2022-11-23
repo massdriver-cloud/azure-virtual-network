0efe0a014fb7d627b1b8dde0b046c6d8a81f647d
Michael Lacore <michael@massdriver.cloud>
2022-11-23 12:25:22 -0700

    Removing unused service endpoints

--- src/main.tf
+++ src/main.tf
@@ -19,5 +19,5 @@ resource "azurerm_subnet" "main" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = [cidrsubnet(var.cidr, 1, 0)]
-  service_endpoints    = ["Microsoft.Storage", "Microsoft.Sql", "Microsoft.KeyVault", "Microsoft.ContainerRegistry"]
+  service_endpoints    = ["Microsoft.Storage"]
 }
